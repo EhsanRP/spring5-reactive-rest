2217beb579d23f76f3c32bd90c435b83ecaf8e88
EhsanRP <sir.ehrp@gmail.com>
2020-12-29 20:47:31 +0330

    Project Init

---
+++ src/main/java/guru/springframework/spring5reactiverest/Spring5ReactiveRestApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5reactiverest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5ReactiveRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5ReactiveRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/spring5reactiverest/Spring5ReactiveRestApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5reactiverest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5ReactiveRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
